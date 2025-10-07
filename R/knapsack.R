21efdda2f92f06629cf990134fd7d80c66a5a5af
Cl4ryty <61427823+Cl4ryty@users.noreply.github.com>
2025-10-07 20:07:25 +0200

    tasks 1.1.1-1.1.5

---
+++ R/knapsack.R
@@ -0,0 +1,146 @@
+brute_force_knapsack <- function(x, W, parallel=FALSE){
+  if(!is.data.frame(x)){
+    stop("x must be a data.frame")
+  }
+  if(!("v" %in% names(x)) || !("w" %in% names(x))){
+    stop("x must contain the variables v and w")
+  }
+  if(any(x<=0)){
+    stop("x must contain only positive values")
+  }
+
+  if(W%%1 != 0 || W <=0){
+    stop("W must be a positive whole number")
+  }
+
+  # get vector of numbers 1:2^n
+  n <- nrow(x)
+  numbers <- c(1:2^n)
+  # get binary representations
+  binary <- intToBits(numbers)
+  bit_matrix <- matrix(binary, ncol = length(binary)/length(numbers), byrow=TRUE)
+  # get only the columns relevant for us
+  bit_matrix <- bit_matrix[,1:n]
+
+  values <- rep(-Inf, n)
+  weights <- rep(Inf, n)
+  if(parallel){
+    values <- parallel::mclapply(bit_matrix, 1, function(row){
+      sum(x$v[row==1])
+    })
+    weights <- parallel::mclapply(bit_matrix, 1, function(row){
+      sum(x$w[row==1])
+    })
+  }else{
+    values <- apply(bit_matrix, 1, function(row){
+      sum(x$v[row==1])
+    })
+    weights <- apply(bit_matrix, 1, function(row){
+      sum(x$w[row==1])
+    })
+  }
+
+
+  # set values with too high weights to negative infinity
+  values[weights > W] = -Inf
+
+  # get the row with the highest value
+  best_combination_index = which(values==max(values))
+
+  # get the indices of the elements in the combination for this best value
+  elements = which(bit_matrix[best_combination_index,]==1)
+
+  # return the value and the elements
+  list(value=max(values), elements=elements)
+
+ }
+
+knapsack_dynamic <- function(x, W){
+  if(!is.data.frame(x)){
+    stop("x must be a data.frame")
+  }
+  if(!("v" %in% names(x)) || !("w" %in% names(x))){
+    stop("x must contain the variables v and w")
+  }
+  if(any(x<=0)){
+    stop("x must contain only positive values")
+  }
+
+  if(W%%1 != 0 || W <=0){
+    stop("W must be a positive whole number")
+  }
+  n <- nrow(x)
+  m <- matrix(data=0, nrow=n+1, ncol=W+1)
+  # browser()
+  for(i in c(1:n+1)){
+    for(j in c(1:W+1)){
+      if (x$w[i-1] > j){
+        m[i, j] <- m[i-1, j]
+      }else{
+        m[i, j] <- max(m[i-1, j], m[i-1, j-x$w[i-1]] + x$v[i-1])
+      }
+    }
+  }
+
+  knapsack <- function(i, j){
+    # browser()
+    if(i == 1){
+      return()
+    }
+    if(m[i, j] > m[i-1, j]){
+      return(c(i-1, knapsack(i-1, j-x$w[i-1])))
+    }else{
+      return(knapsack(i-1, j))
+    }
+  }
+  # return list of results, reverse elements so that they are in the same order
+  # as for the other functions
+  return(list(value=max(m), elements=rev(knapsack(n+1, W+1))))
+
+}
+
+
+greedy_knapsack <- function(x, W){
+  if(!is.data.frame(x)){
+    stop("x must be a data.frame")
+  }
+  if(!("v" %in% names(x)) || !("w" %in% names(x))){
+    stop("x must contain the variables v and w")
+  }
+  if(any(is.na(x))){
+    stop("x must contain only positive values")
+  }
+
+  if(any(x<=0)){
+    stop("x must contain only positive values")
+  }
+
+  if(W%%1 != 0 || W <=0){
+    stop("W must be a positive whole number")
+  }
+  n <- nrow(x)
+  x$i <- c(1:nrow(x))
+  x$unit_per_weight <- x$v/x$w
+  x <- sort_by(x, x$unit_per_weight, decreasing=TRUE)
+  x$cumsum <- cumsum(x$w)
+  x$cumsum[x$cumsum>W] <- -Inf
+
+  value=sum(x$v[x$cumsum>-Inf])
+  elements = sort(x$i[x$cumsum>-Inf])
+  # weight_left = W-sum(x$w[x$cumsum>-Inf])
+  # x$w[x$cumsum>-Inf] <- Inf
+#
+#   while(any(x$w<=weight_left)){
+#     greedy_index <- where(x$w<=weight_left)[1]
+#     value <- x$v[greedy_index]
+#     elements <-  c(elements, x$i[greedy_index])
+#     weight_left <- weight_left-x$w[greedy_index]
+#     x$w[greedy_index] <- -Inf
+#   }
+  return(list(value=value, elements=elements))
+
+}
+
+
+x <- data.frame(v=c(5,4,3,2), w=c(4,3,2,1))
+x
---
+++ README.md
@@ -0,0 +1,25 @@
+
+# AdvancedRHT2025Lab06
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of AdvancedRHT2025Lab06 is to ...
+
+## Installation
+
+You can install the development version of AdvancedRHT2025Lab06 like so:
+
+``` r
+# FILL THIS IN! HOW CAN PEOPLE INSTALL YOUR DEV PACKAGE?
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(AdvancedRHT2025Lab06)
+## basic example code
+```
+
